b9463863825c1c6c00c23b5bc637ba6d494f1c41
Tomáš Zezula <tomas.zezula@avast.com>
2022-07-07 17:39:06 +0200

    feat: Ktor client retry.

--- api/src/main/kotlin/com/tomaszezula/make/Application.kt
+++ api/src/main/kotlin/com/tomaszezula/make/Application.kt
@@ -7,36 +7,56 @@ import com.tomaszezula.make.plugins.configureRouting
 import com.typesafe.config.ConfigFactory
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.network.sockets.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.logging.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
-import org.slf4j.LoggerFactory
 import java.net.URI
 
 fun main() {
     val config = HoconApplicationConfig(ConfigFactory.load())
+    val client = HttpClient(CIO) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
+        }
+        install(HttpRequestRetry) {
+            maxRetries = config.maxRetries()
+            retryIf { _, response ->
+                !response.status.isSuccess()
+            }
+            retryOnExceptionIf { _, cause ->
+                cause.isTimeoutException()
+            }
+            exponentialDelay()
+        }
+    }
+    val makeConfig = MakeConfig(
+        baseUrl = URI(config.property("make.url").getString()),
+        token = AuthToken(config.property("make.auth.token").getString())
+    )
     embeddedServer(Netty, port = config.port, host = config.host) {
         install(ContentNegotiation) {
             json()
         }
         configureRouting(
-            CreateScenarioHandler(
-                HttpClient(CIO) {
-                    install(Logging) {
-                        logger = Logger.DEFAULT
-                        level = LogLevel.ALL
-                    }
-                },
-                MakeConfig(
-                    baseUrl = URI(config.property("make.url").getString()),
-                    token = AuthToken(config.property("make.auth.token").getString())
-                ),
-                LoggerFactory.getLogger(CreateScenarioHandler::class.java)
-            )
+            CreateScenarioHandler(client, makeConfig)
         )
     }.start(wait = true)
+}
+
+private fun HoconApplicationConfig.maxRetries(): Int =
+    this.propertyOrNull("ktor.client.retry.max")?.getString()?.toInt() ?: 5
+
+private fun Throwable.isTimeoutException(): Boolean = when (this) {
+    is HttpRequestTimeoutException -> true
+    is ConnectTimeoutException -> true
+    is SocketTimeoutException -> true
+    else -> false
 }
